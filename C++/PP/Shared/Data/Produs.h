35158c0f8f2380d9bd6d93b36a13ef4e82894cae
kickhead13 <75039938+kickhead13@users.noreply.github.com>
2023-06-29 22:53:52 +0300

    Update Produs.h
    Acum clasa Produs mostenste clasa Produs_Simplu (ma va ajuta la cos de cumparaturi)

--- C++/PP/Shared/Data/Produs.h
+++ C++/PP/Shared/Data/Produs.h
@@ -1,6 +1,7 @@
 #ifndef LIBS
 #define LIBS
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <string.h>
 #endif
@@ -10,8 +11,18 @@
 #include "Categorie.h"
 #endif
 
-class Produs{
-        std::string cod_de_bare;
+class Produs_Simplu {
+    std::string cod_de_bare;
+    int cantitate;
+public:
+    std::string getCodDeBare() const{return this->cod_de_bare;};
+    int getCantitate() const{return this->cantitate;};
+
+    void setCodDeBare(const std::string cod_de_bare){this -> cod_de_bare = cod_de_bare;};
+    void setCantitate(const int cantitate){this -> cantitate = cantitate;};
+};
+
+class Produs : public Produs_Simplu{
         std::string denumire;
         int cantitate;
         double pret;
@@ -21,26 +32,25 @@ class Produs{
         Produs(const Produs&);
         ~Produs();
 
-        std::string getCodDeBare() const{return this->cod_de_bare;};
+
         std::string getDenumire() const{return this->denumire;};
-        int getCantitate() const{return this->cantitate;};
         double getPret() const{return this->pret;};
         Categorie* getCategorie() const{return this -> categorie;};
 
-        void setCodDeBare(const std::string cod_de_bare){this -> cod_de_bare = cod_de_bare;};
         void setDenumire(const std::string denumire){this -> denumire = denumire;};
-        void setCantitate(const int cantitate){this -> cantitate = cantitate;};
         void setPret(const double pret){this -> pret = pret;};
         void setCategorie(const Categorie &categorie){if(this -> categorie != NULL){delete this -> categorie;}this -> categorie = new Categorie(categorie);};
 
         Produs operator=(const Produs&);
         friend std::ostream& operator<<(std::ostream &, const Produs&);
         friend std::istream& operator>>(std::istream &, Produs&);
+        friend std::ifstream& operator>>(std::ifstream &, Produs&);
+        friend std::ofstream& operator<<(std::ofstream &, const Produs&);
 
         friend void adaugare_produs(const std::string, const std::string, const int, const double, const std::string);
         friend void modificare_produs(const std::string, const int);
         friend void modificare_produs(const std::string, const std::string, const std::string);
-        friend void stergere_produs(const std::string); //implementare in App  
+        friend void stergere_produs(const std::string); //implementare in App
 
     private:
         void init(const std::string, const std::string, const int, const double, const Categorie&);
