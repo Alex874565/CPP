32d94ca6a6a6991f9d54b084099cef32bc754f1c
kickhead13 <75039938+kickhead13@users.noreply.github.com>
2023-07-01 20:25:30 +0300

    Update Produs.h

--- C++/PP/Shared/Data/Produs.h
+++ C++/PP/Shared/Data/Produs.h
@@ -21,6 +21,9 @@ public:
 
     void setCodDeBare(const std::string cod_de_bare){this -> cod_de_bare = cod_de_bare;};
     void setCantitate(const int cantitate){this -> cantitate = cantitate;};
+
+    template <typename T> friend T& operator>>(T&, Produs_Simplu&);
+    template <typename T> friend T& operator<<(T&, const Produs_Simplu&);
 };
 
 class Produs : public Produs_Simplu{
